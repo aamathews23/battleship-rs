34bca4f289102b96c41a7e21da065cbf56b58ec1
Aaron Mathews <aamathews23@gmail.com>
2025-01-04 11:24:15 -0800

    add vue project for wasm

---
+++ battleship_web/www/prettier.config.js
@@ -0,0 +1,10 @@
+const config = {
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 100,
+  endOfLine: 'auto',
+  singleAttributePerLine: true,
+};
+
+export default config;
